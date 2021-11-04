82b967c1bbc6d0aeb2c946055d9267c365f0e450
Michael Greene <profmikegreene@gmail.com>
2021-11-03 21:53:08 -0400

    Initialize SAK-45142

---
+++ webcomponents/tool/src/main/frontend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": { "browser": true, "es2021": true },
+  "globals": {
+    "$": "readonly",
+    "portal": "readonly",
+    "CKEDITOR": "readonly",
+    "moment": "readonly",
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": { "ecmaVersion": 12, "sourceType": "module" },
+  "rules": {
+    "indent": ["warn", 2, { "SwitchCase": 1, "MemberExpression": "off", "ignoredNodes": ["TemplateLiteral > *"] }],
+    "linebreak-style": ["warn","unix"],
+    "semi": ["warn", "always"],
+    "no-unused-vars": "warn",
+    "no-undef": "warn",
+    "no-useless-escape": "warn",
+    "no-redeclare": "warn",
+    "no-cond-assign": "warn",
+    "no-empty": "warn",
+  }
+};
